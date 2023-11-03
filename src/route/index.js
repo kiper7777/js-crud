1e0d45689e73947cebbc1a18d85888bf80336d39
Dmytro Kiperman <8boyss@ukr.net>
2023-11-03 23:05:02 +0100

    upd

--- src/route/index.js
+++ src/route/index.js
@@ -87,36 +87,6 @@ Product.add(
   10,
 )
 
-Product.add(
-  'https://picsum.photos/200/300',
-  'Компютер Artline Gaming (X43v31) AMD Ryzen 5 3600/',
-  'AMD Ryzen 5 3600 (3.6 - 4.2 ГГц) / RAM 16 ГБ / HDD 1 ТБ + SSD 480 ГБ / nVidia GeForce RTX 3050, 8 ГБ / без ОД / LAN / без ОС',
-  [
-    { id: 1, text: 'Готовий до відправки' },
-    { id: 2, text: 'Топ продажів' },
-  ],
-  27000,
-  10,
-)
-
-Product.add(
-  'https://picsum.photos/200/300',
-  'Компютер COBRA Advanced (I11F.8.H1S2.15T.13356) Intel',
-  'Intel Core i3-10100F (3.6 - 4.3 ГГц) / RAM 8 ГБ / HDD 1 ТБ + SSD 240 ГБ / GeForce GTX 1050 Ti, 4 ГБ / без ОД / LAN / Linux',
-  [{ id: 2, text: 'Топ продажів' }],
-  20000,
-  10,
-)
-
-Product.add(
-  'https://picsum.photos/200/300',
-  'Компютер ARTLINE Gaming by ASUS TUF v119 (TUFv119)',
-  'Intel Core i9-13900KF (3.0 - 5.8 ГГц) / RAM 64 ГБ / SSD 2 ТБ (2 x 1 ТБ) / nVidia GeForce RTX 4070 Ti, 12 ГБ / без ОД / LAN / Wi-Fi / Bluetooth / без ОС',
-  [{ id: 1, text: 'Готовий до відправки' }],
-  40000,
-  10,
-)
-
 class Purchase {
   static DELIVERY_PRICE = 150
   static #BONUS_FACTOR = 0.1
